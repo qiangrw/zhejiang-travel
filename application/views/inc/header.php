3881f0c8c0cc6b6052f3165337e6f6afcedf3ead
Runwei Qiang <qiangrw@gmail.com>
2015-02-10 16:18:49 +0800

    fix assets location.

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>浙江旅游</title>
     <link rel="shortcut icon" href="favicon.ico">
-    <link rel="stylesheet" href="css/themes/default/jquery.mobile-1.4.5.min.css">
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery.mobile-1.4.5.min.js"></script>
+    <link rel="stylesheet" href="<?= base_url('css/themes/default/jquery.mobile-1.4.5.min.css') ?>">
+    <script src="<?= base_url('js/jquery.js') ?>"></script>
+    <script src="<?= base_url('js/jquery.mobile-1.4.5.min.js') ?>"></script>
 </head>
 
 <body>
