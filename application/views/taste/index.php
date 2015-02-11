51ba6701bd0abad84d7d5e95f20efcb1cc117b78
LU, Hongchi <hongchilu@yahoo.com>
2015-02-11 21:09:31 +0800

    finish 这里吃

--- application/views/taste/index.php
+++ application/views/taste/index.php
@@ -1,4 +1,75 @@
-<h1>浙里吃</h1>
-<p>浙里吃</p>
-
-	
+<div style="background: url('<?= base_url('img/basic-bg.jpg') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
+    <div>
+        <img src="<?= base_url('img/taste/dpr.jpg') ?>" alt="" style="width: 100%%; margin: 0 0 15px;"/>
+    </div>
+    <div class="container">
+        <div class="thumb-list">
+            <div class="thumb-item">
+                <img class="thumbnail" src="<?= base_url('img/stay/stay1.png') ?>" alt=""/>
+                <div class="info">
+                    <h4 class="title">东坡肉</h4>
+                    <p class="location">XXX小区内</p>
+                    <p class="comment">123 / test</p>
+                    <p class="des">南尖岩位于监督法了解对方垃圾感觉地方分上减肥撒开飞机撒娇福建犯困睡觉啊福建省风飒风</p>
+                </div>
+                <div class="price">
+                    <span class="appendix">$</span><strong class="number">108</strong><span class="appendix"> 起</span>
+                    <br/>
+                    <span class="addon">特别优惠噢噢噢噢噢</span>
+                </div>
+            </div>
+            <div class="thumb-item">
+                <img class="thumbnail" src="<?= base_url('img/stay/stay2.png') ?>" alt=""/>
+                <div class="info">
+                    <h4 class="title">东坡肉</h4>
+                    <p class="location">XXX小区内</p>
+                    <p class="comment">123 / test</p>
+                    <p class="des">南尖岩位于监督法了解对方垃圾感觉地方分上减肥撒开飞机撒娇福建犯困睡觉啊福建省风飒风</p>
+                </div>
+                <div class="price">
+                    <span class="appendix">$</span><strong class="number">80</strong><span class="appendix"> 起</span>
+                    <br/>
+                    <span class="addon">特别优惠噢噢噢噢噢</span>
+                </div>
+            </div>
+            <div class="thumb-item">
+                <img class="thumbnail" src="<?= base_url('img/stay/stay1.png') ?>" alt=""/>
+                <div class="info">
+                    <h4 class="title">东坡肉</h4>
+                    <p class="location">XXX小区内</p>
+                    <p class="comment">123 / test</p>
+                    <p class="des">南尖岩位于监督法了解对方垃圾感觉地方分上减肥撒开飞机撒娇福建犯困睡觉啊福建省风飒风</p>
+                </div>
+                <div class="price">
+                    <span class="appendix">$</span><strong class="number">78</strong><span class="appendix"> 起</span>
+                    <br/>
+                    <span class="addon">特别优惠噢噢噢噢噢</span>
+                </div>
+            </div>
+            <div class="thumb-item">
+                <img class="thumbnail" src="<?= base_url('img/stay/stay2.png') ?>" alt=""/>
+                <div class="info">
+                    <h4 class="title">东坡肉</h4>
+                    <p class="location">XXX小区内</p>
+                    <p class="comment">123 / test</p>
+                    <p class="des">南尖岩位于监督法了解对方垃圾感觉地方分上减肥撒开飞机撒娇福建犯困睡觉啊福建省风飒风</p>
+                </div>
+                <div class="price">
+                    <span class="appendix">$</span><strong class="number">67</strong><span class="appendix"> 起</span>
+                    <br/>
+                    <span class="addon">特别优惠噢噢噢噢噢</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/taste/taste_bg.png') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
+    <div>
+        <img src="<?= base_url('img/taste/taste_logo.png') ?>" alt="诗画浙江" style="width: 50%; float: right; margin: 10px;"/>
+    </div>
+</div>
+<script>
+    $('.cover').on('click', function (){
+        $(this).fadeOut(500);
+    });
+</script>
