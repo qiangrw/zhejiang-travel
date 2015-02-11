86da4022801a98b45b6cf4d0234f67ceec386f17
Runwei Qiang <qiangrw@gmail.com>
2015-02-11 23:02:33 +0800

    init buy page.

--- application/views/buy/index.php
+++ application/views/buy/index.php
@@ -1,4 +1,10 @@
-<h1>浙里买</h1>
-<p>浙里买</p>
+<div class="" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/buy/buy_bg.png') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
 
-	
+</div>
+<div class="cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/buy/buy_cover_after.png') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
+</div>
+<script>
+    $('.cover').on('click', function (){
+        $(this).fadeOut(500);
+    });
+</script>
--- application/views/play/index.php
+++ application/views/play/index.php
@@ -1,4 +1,4 @@
-<div class="" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/play/play') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
+<div class="" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/play/play.jpg') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
 
 </div>
 <div class="cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/play/play_cover.jpg') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
