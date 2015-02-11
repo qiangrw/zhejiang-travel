23c7eb47dd3b5c5a4c249febdc85674900189f54
LU, Hongchi <hongchilu@yahoo.com>
2015-02-11 14:01:27 +0800

    page updates

--- application/views/release/index.php
+++ application/views/release/index.php
@@ -1,4 +1,8 @@
-<h1>浙里发</h1>
-<p>浙里发</p>
-
-	
+<div style="background: url('<?= base_url('img/travel-bg.jpg') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
+    <img src="<?= base_url('img/release.jpg') ?>" alt="cover" style="margin:0; width: 100%;" id="release-cover"/>
+</div>
+<script type="text/javascript">
+    $('#release-cover').on('click', function (){
+        $(this).fadeOut(500);
+    });
+</script>
--- application/views/travel/index.php
+++ application/views/travel/index.php
@@ -1,7 +1,10 @@
-<div style="background: url('<?= base_url('img/travel-cover.jpg') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
-    <a href="<?= site_url("home") ?>" id="goToHome" style="display: none"></a>
+<div style="background: url('<?= base_url('img/travel-bg.jpg') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
+    <img src="<?= base_url('img/travel-cover.jpg') ?>" alt="cover" style="margin:0; width: 100%;" id="travel-cover"/>
 </div>
 <script type="text/javascript">
+    $('#travel-cover').on('click', function (){
+        $(this).fadeOut(500);
+    });
     if (window.DeviceMotionEvent) {
         window.addEventListener('devicemotion',deviceMotionHandler, false);
     }
