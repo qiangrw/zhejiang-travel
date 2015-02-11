0570d4c898fad9911a93389f7a2b383096614f2e
LU, Hongchi <hongchilu@yahoo.com>
2015-02-11 18:56:25 +0800

    image fix
    finish the release demo

--- application/views/release/index.php
+++ application/views/release/index.php
@@ -15,6 +15,12 @@
                 <hr/>
                 <img src="<?= base_url('img/demo/tmp1.png') ?>" alt="" style="width: 100%"/>
             </div>
+            <div class="flat-card">
+                <h4 class="name">南尖岩 <small>景区级别: AAAAA</small></h4>
+                <p class="des">南尖岩位于监督法了解对方垃圾感觉地方分上减肥撒开飞机撒娇福建犯困睡觉啊福建省风飒风</p>
+                <hr/>
+                <img src="<?= base_url('img/demo/tmp1.png') ?>" alt="" style="width: 100%"/>
+            </div>
         </div>
     </div>
     <div id="release-cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/release.jpg') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
