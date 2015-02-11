b1b9e8212396d01083fc5cbadf516ff155eaefa5
Runwei Qiang <qiangrw@gmail.com>
2015-02-11 18:21:45 +0800

    minor change to image

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -1,8 +1,8 @@
-<div style="background: url('<?= base_url('img/welcome.jpg') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
+<div style="background: url('<?= base_url('img/welcome.png') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
     <a href="<?= site_url("home") ?>" id="goToHome" style="display: none"></a>
 </div>
 <script type="text/javascript">
     setTimeout(function () {
         $('#goToHome').trigger('click');
-    }, 5000)
+    }, 2000)
 </script>
