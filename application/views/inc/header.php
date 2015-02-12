9d7958f632430d5d7cab9ce9029f52458b6ebd44
LU, Hongchi <hongchilu@yahoo.com>
2015-02-12 22:56:54 +0800

    jquery minimized

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="<?= base_url('css/themes/default/jquery.mobile-1.4.5.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-    <script src="<?= base_url('js/jquery.js') ?>"></script>
+    <script src="<?= base_url('js/jquery.min.js') ?>"></script>
     <script src="<?= base_url('js/jquery.mobile-1.4.5.min.js') ?>"></script>
 </head>
 <body>
