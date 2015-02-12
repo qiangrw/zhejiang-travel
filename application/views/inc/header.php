0a271bb456c6493fa3340dd7a7cff68ea78b84d9
Runwei Qiang <qiangrw@gmail.com>
2015-02-12 15:34:00 +0800

    update header

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>浙江旅游</title>
+    <title>诗画浙江</title>
     <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="<?= base_url('css/themes/default/jquery.mobile-1.4.5.min.css') ?>">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
