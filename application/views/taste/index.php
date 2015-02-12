434f16242e0b225f171fbb130e5d3ddd336fbcfc
LU, Hongchi <hongchilu@yahoo.com>
2015-02-12 23:40:48 +0800

    opt taste
    add flip option to the 孔明灯

--- application/views/taste/index.php
+++ application/views/taste/index.php
@@ -28,10 +28,40 @@
         <img src="<?= base_url('img/taste/taste_logo.png') ?>" alt="诗画浙江" style="width: 50%; float: right; margin: 10px;"/>
     </div>
 </div>
-<div class="cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/taste/taste_cover.jpg') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
+<div class="cover" id="taste-cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/taste/taste_cover.jpg') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
 </div>
 <script>
     $('.cover').on('click', function (){
         $(this).fadeOut(500);
     });
+</script>
+
+<script type="text/javascript">
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion',deviceMotionHandler, false);
+    }
+
+    var SHAKE_THRESHOLD = 300;
+    var x, y, z, last_x, last_y, last_z;
+    var lastUpdate = 0;
+    function deviceMotionHandler(eventData) {
+        var acceleration =eventData.accelerationIncludingGravity;
+        var curTime = new Date();
+        curTime = curTime.getTime();
+        if ((curTime - lastUpdate)> 100) {
+            var diffTime = curTime -lastUpdate;
+            lastUpdate = curTime;
+            x = acceleration.x;
+            y = acceleration.y;
+            z = acceleration.z;
+            var speed = Math.abs(x +y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+            if (speed > SHAKE_THRESHOLD) {
+                $('#taste-cover').trigger('click')
+            }
+            last_x = x;
+            last_y = y;
+            last_z = z;
+        }
+    }
 </script>
