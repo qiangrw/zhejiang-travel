c75264864672ca1268fba3c91beb124cefccdd28
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 01:00:49 +0800

    add home-back btn in taste view

--- application/views/taste/index.php
+++ application/views/taste/index.php
@@ -1,3 +1,37 @@
+ <style>
+        #toolBackTo {
+            position: fixed;
+            left: 13%;
+            bottom: 20px;
+            width: 50px;
+            height: 75px;
+            z-index: 11;
+        }
+        #toolBackTo a {
+            display: block;
+            width: 50px;
+            height: 50px;
+            text-align: center;
+            background-color: #a4aab4;
+            background-color: rgba(164,170,180,0.5);
+            background-position: 16px 19px;
+            border-radius: 2px;
+        }
+        #toolBackTo .home-back {
+            text-decoration: none;
+            font-weight: normal;
+            text-shadow: none;
+            background-image: none;
+            padding-top: 4px;
+            height: 21px;
+            color: white;
+            letter-spacing: 2px;
+            margin-top: 1px;
+            text-align: center;
+            position: absolute;
+            top: 50px;
+        }
+</style>    
 <div style="background: url('<?= base_url('img/basic-bg.jpg') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
     <div>
         <img src="<?= base_url('img/taste/dpr.jpg') ?>" alt="" style="width: 100%%; margin: 0 0 15px;"/>
@@ -21,6 +55,11 @@
             </div>
             <?php endforeach; ?>
         </div>
+
+        
+        <div id="toolBackTo" class="back-to" style="left: 80%; display:none;">
+        <a href="<?= site_url('home') ?>" class="home-back" style="display: block;">首页</a>
+        </div>
     </div>
 </div>
 <div class="cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/taste/taste_bg.png') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
@@ -40,6 +79,7 @@
         $("#hand").animate( { top: "-=6%" }, 500); 
         $("#hand").animate( { top: "+=6%" }, 500); 
         $("#hand").animate( { top: "-=6%" }, 500); 
+        $("#toolBackTo").show();
     });
 </script>
 
