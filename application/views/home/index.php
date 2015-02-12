3768aba225563429c16bcac8662a78854df0e2b1
LU, Hongchi <hongchilu@yahoo.com>
2015-02-12 13:38:24 +0800

    image and home updates
    Make the animation better; optimize the picture size; bug fix for the
    home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,26 +1,27 @@
-<div style="background: url('<?= base_url('img/home-bg.jpg') ?>') no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0; background-position: center bottom">
+<div class="master">
+    <div class="master-img" style="background: url('<?= base_url('img/home-bg.jpg') ?>') center; background-size: 100% auto;"></div>
     <img src="<?= base_url('img/shzj.png') ?>" alt="诗画浙江" style="position: absolute; right: 10px; top: 5px; width: 120px;"/>
     <div id="home-ctrl-wrapper" style="position: absolute; left: 0; right: 0; bottom: 0; height: 300px;">
         <div id="home-ctrl" style="width: 100%; height: 100%; position: relative;">
-            <a class="nav-dot" href="<?= site_url("travel") ?>" style="position: absolute; bottom: 158px; left: 12%; width: 90px;">
+            <a class="nav-dot" href="<?= site_url("travel") ?>" style="position: absolute; bottom: 40%; left: 6%; width: 25%;">
                 <img src="<?= base_url('img/home/zly.png') ?>" alt="浙里游" style="width: 100%;"/>
             </a>
-            <a class="nav-dot" href="<?= site_url("release") ?>" style="position: absolute; bottom: 219px; left: 36%; width: 78px; height: 79px;">
-                <img src="<?= base_url('img/home/zlf.png') ?>" alt="浙里发" style="width: 100%;"/>
-            </a>
-            <a class="nav-dot" href="<?= site_url("stay") ?>" style="position: absolute; bottom: 194px; left: 62%; width: 120px; height: 125px;">
+<!--            <a class="nav-dot" href="--><?//= site_url("release") ?><!--" style="position: absolute; bottom: 65%; left: 36%; width: 78px; height: 79px;">-->
+<!--                <img src="--><?//= base_url('img/home/zlf.png') ?><!--" alt="浙里发" style="width: 100%;"/>-->
+<!--            </a>-->
+            <a class="nav-dot" href="<?= site_url("stay") ?>" style="position: absolute; bottom: 50%; left: 50%; width: 32%;">
                 <img src="<?= base_url('img/home/zlz.png') ?>" alt="浙里住" style="width: 100%;"/>
             </a>
-            <a class="nav-dot" href="<?= site_url("buy") ?>" style="position: absolute; bottom: 38px; left: 19%; width: 78px; height: 85px;">
+            <a class="nav-dot" href="<?= site_url("buy") ?>" style="position: absolute; bottom: 5%; left: 15%; width: 15%;">
                 <img src="<?= base_url('img/home/zlm.png') ?>" alt="浙里买" style="width: 100%;"/>
             </a>
-            <a class="nav-dot" href="<?= site_url("taste") ?>" style="position: absolute; bottom: 116px; left: 69%; width: 58px; height: 60px;">
+            <a class="nav-dot" href="<?= site_url("taste") ?>" style="position: absolute; bottom: 35%; left: 75%; width: 12%;">
                 <img src="<?= base_url('img/home/zlc.png') ?>" alt="浙里吃" style="width: 100%;"/>
             </a>
-            <a class="nav-dot" href="<?= site_url("play") ?>" style="position: absolute; bottom: 38px; left: 59%; width: 70px; height: 73px; ">
+            <a class="nav-dot" href="<?= site_url("play") ?>" style="position: absolute; bottom: 5%; left: 68%; width: 16%;">
                 <img src="<?= base_url('img/home/zlw.png') ?>" alt="浙里玩" style="width: 100%;"/>
             </a>
-            <a class="nav-center" href="#" style="position: absolute; bottom: 60px; left: 28%; width: 160px;">
+            <a class="nav-center" href="#" style="position: absolute; bottom: 12%; left: 28%; width: 40%;">
                 <img src="<?= base_url('img/home/zj.png') ?>" alt="浙里玩" style="width: 100%;"/>
             </a>
         </div>
@@ -30,8 +31,10 @@
     setTimeout(function () {
         var dots = $('.nav-dot');
         for (var i = 0; i < dots.length; i++) {
+            $(dots[i]).css('transition-delay', Math.random() * Math.random() * 1.5 + "s");
             $(dots[i]).addClass('active')
         }
+        $('.master-img').fadeIn(800);
         $('.nav-center').addClass('active')
     }, 1000)
 </script>
--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -9,31 +9,5 @@
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
     <script src="<?= base_url('js/jquery.js') ?>"></script>
     <script src="<?= base_url('js/jquery.mobile-1.4.5.min.js') ?>"></script>
-    <style type="text/css">
-        .place {
-            margin-bottom: 12%;
-        }
-        .buy-button-div {
-            float:right;
-            margin-right: 10%;
-            margin-top: -4%;
-            margin-button: 4%;
-        }
-        .buy-button, .buy-button:hover, .buy-button:visited, .buy-button:active {
-            font-family: "华文仿宋", "宋体";
-            font-size: 1em;
-            color: white!important;
-            font-weight: normal!important;
-            background-color: black;
-            width: 200px;
-            height: 50px;
-            padding: 8px 30px;
-            text-decoration: none;
-            text-shadow: none;
-            border-radius: 25px;
-            z-index: 999;
-        }
-        .buy-button:hover { cursor: pointer; }
-    </style>
 </head>
 <body>
