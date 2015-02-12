3988fb2d90e10ff2a15f1915f9326acdd3f9aa9c
Runwei Qiang <qiangrw@gmail.com>
2015-02-12 22:49:58 +0800

    provide stay information from backend

--- application/controllers/stay.php
+++ application/controllers/stay.php
@@ -6,11 +6,31 @@ class Stay extends CI_Controller {
 		$this->load->view('inc/header');
         $this->load->view('stay/index');
         $this->load->view('inc/footer');
-	}
+    }
+
+    /**
+     * The detail of the stay information
+     */
     public function detail()
-    {
+    {   
+        // TODO get data from database        
+        $data['items'] = array();
+        $item = (object)(array(
+            'thumbnail' => base_url('img/stay/stay1.png'),
+            'title' => '乌镇测试住宅',
+            'location' => 'XXXX小区内',
+            'comment' => 'This is the comment.',
+            'description' => '南尖岩位于监督法了解对方垃圾感觉地方分上减肥撒开飞机撒娇福建犯困睡觉啊福建省风飒风',
+            'price' => 108,
+            'addon' => '特别优惠噢噢噢'
+        ));
+        $data['items'] []= $item;
+        $data['items'] []= $item;
+        $data['items'] []= $item;
+        $data['items'] []= $item;
+
         $this->load->view('inc/header');
-        $this->load->view('stay/detail');
+        $this->load->view('stay/detail', $data);
         $this->load->view('inc/footer');
     }
 }
