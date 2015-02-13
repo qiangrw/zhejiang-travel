26876e98a160d32ead6597321c3e83f197265388
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 11:18:49 +0800

    Fix #2, beautify notice in each page.

--- application/views/travel/index.php
+++ application/views/travel/index.php
@@ -1,7 +1,7 @@
 <div class="cover" style="background: url('<?= base_url('img/travel-cover.jpg') ?>') center no-repeat; background-size: 100% auto">
 <div id="hint" style="display:none">
 <center>
-<p style="font-size: 0.8em; font-color: grey; text-shadow: 2px; padding-top: 10px;">晃动手机，摇曳喜糖垂杨柳</p>
+<p style="font-size: 1.2em; color: green; text-shadow: 5px 5px 5px #FFF; padding-top: 70%;">晃动手机，摇曳喜糖垂杨柳</p>
 </center>
 </div>
 </div>
