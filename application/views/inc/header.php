0cc97af8220c6e0a39b2b4a76141a75831a1abef
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 17:25:03 +0800

    use cdn jquery

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -7,7 +7,8 @@
     <link rel="shortcut icon" href="favicon.ico">
     <!--<link rel="stylesheet" href="<?= base_url('css/themes/default/jquery.mobile-1.4.5.min.css') ?>">-->
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-    <script src="<?= base_url('js/jquery.min.js') ?>"></script>
+    <!--<script src="<?= base_url('js/jquery.min.js') ?>"></script>-->
+    <script src="http://cdnjscn.b0.upaiyun.com/libs/jquery/1.11.1-rc2/jquery.min.js"</script>
 <!--    <script src="--><?//= base_url('js/jquery.mobile-1.4.5.min.js') ?><!--"></script>-->
 </head>
 <body>
