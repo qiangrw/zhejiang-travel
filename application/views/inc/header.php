2b7226e1f7bac955d3e0d4ebeb84751475147a8a
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 17:13:10 +0800

    delete jquery mobile css

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>诗画浙江</title>
     <link rel="shortcut icon" href="favicon.ico">
-    <link rel="stylesheet" href="<?= base_url('css/themes/default/jquery.mobile-1.4.5.min.css') ?>">
+    <!--<link rel="stylesheet" href="<?= base_url('css/themes/default/jquery.mobile-1.4.5.min.css') ?>">-->
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
     <script src="<?= base_url('js/jquery.min.js') ?>"></script>
 <!--    <script src="--><?//= base_url('js/jquery.mobile-1.4.5.min.js') ?><!--"></script>-->
