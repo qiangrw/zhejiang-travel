a46b5e27a07884015b73ff42d94825b70e05788e
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 16:09:19 +0800

    update welcome figure.

--- application/views/welcome/index.php
+++ application/views/welcome/index.php
@@ -1,4 +1,4 @@
-<div style="background: url('<?= base_url('img/welcome.png') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
+<div style="background: url('<?= base_url('img/welcome.jpg') ?>') center no-repeat; background-size: 100% auto; position: absolute; top: 0; bottom: 0; left: 0; right: 0">
 </div>
 <script type="text/javascript">
     setTimeout(function () {
