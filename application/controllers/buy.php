91c41ffac12a75422f0e204b849c6257c5ef4864
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 16:54:43 +0800

    add buy page fake data

--- application/controllers/buy.php
+++ application/controllers/buy.php
@@ -3,9 +3,23 @@
 class Buy extends CI_Controller {
 
 	public function index()
-	{
+    {
+        // TODO get data from database        
+        $data['items'] = array();
+        $item = (object)(array(
+            'thumbnail' => base_url('img/buy/item_div.png'),
+            'title' => '古代油纸伞',
+            'price' => 40,
+            'description' => '这是推荐理由，这个伞特别好，特别好，特别好哟特别好用'
+        ));
+        $data['items'] []= $item;
+        $data['items'] []= $item;
+        $data['items'] []= $item;
+        $data['items'] []= $item;
+
+
 		$this->load->view('inc/header');
-        $this->load->view('buy/index');
+        $this->load->view('buy/index', $data);
         $this->load->view('inc/footer');
 	}
 }
