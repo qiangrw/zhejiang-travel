7cd2f2e5bf8a7be38bdd3eb88745a974f44742f7
Runwei Qiang <qiangrw@gmail.com>
2015-02-14 00:23:56 +0800

    use cdn jquery

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>诗画浙江</title>
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-    <script src="<?= base_url('js/jquery.min.js') ?>"></script>
+    <script src="http://cdnjscn.b0.upaiyun.com/libs/jquery/1.11.1-rc2/jquery.min.js"></script>
 </head>
 <body>
