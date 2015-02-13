3e7300e9c6052c8d63eb1fb0fd010af70bd8f42f
LU, Hongchi <hongchilu@yahoo.com>
2015-02-13 16:57:23 +0800

    fix the buy

--- application/views/buy/index.php
+++ application/views/buy/index.php
@@ -1,8 +1,9 @@
-<div class="" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/buy/buy_bg.png') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
+<div class="cover-static">
     <div>
         <img src="<?= base_url('img/buy/buy_logo.png') ?>" alt="" style="width: 40%; margin: 20px 15px;"/>
         <a href="<?= site_url('home') ?>">
-        <img class="shzj-logo" src="<?= base_url('img/shzj.png') ?>" alt="诗画浙江" style="width: 25%; float: right; margin: 10px;"/>
+            <img class="shzj-logo" src="<?= base_url('img/shzj.png') ?>" alt="诗画浙江"
+                 style="width: 25%; float: right; margin: 10px;"/>
         </a>
     </div>
     <div class="container">
@@ -10,17 +11,15 @@
             <img src="<?= base_url('img/buy/buy_banner.png') ?>" alt="" style="width: 100%"/>
         </div>
         <div id="item-list">
-          <center>
-            <img src="<?= base_url('img/buy/item1.png') ?>" alt="" style="width: 23%"/>
-            <img src="<?= base_url('img/buy/item2.png') ?>" alt="" style="width: 23%"/>
-            <img src="<?= base_url('img/buy/item3.png') ?>" alt="" style="width: 23%"/>
-            <img src="<?= base_url('img/buy/item4.png') ?>" alt="" style="width: 23%"/>
-          </center>
+            <img class="img-item" src="<?= base_url('img/buy/item1.png') ?>" alt=""/>
+            <img class="img-item" src="<?= base_url('img/buy/item2.png') ?>" alt=""/>
+            <img class="img-item" src="<?= base_url('img/buy/item3.png') ?>" alt=""/>
+            <img class="img-item" src="<?= base_url('img/buy/item4.png') ?>" alt=""/>
         </div>
-        
+
         <div id="item-info">
             <img src="<?= base_url('img/buy/item_div.png') ?>" alt="" style="width: 100%"/>
-            <!--<div style="padding-left: 30%;">
+                <!--<div style="padding-left: 30%;">
                 <h2>古代油纸伞</h2>
                 <span>热卖价： 40元</span>
                 <h3>掌柜推荐理由</h3>
@@ -34,19 +33,17 @@
                     <a href="#" class="ui-btn ui-btn-inline ui-mini">手机二维码</a>
                 </div>
             </div>-->
-        </div>   
+        </div>
     </div>
 </div>
-<div class="cover" style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/buy/buy_cover_after.jpg') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
-    <div id="hand" style="background: url('<?= base_url('img/hand.png') ?>') no-repeat; background-size: 30px 30px; height: 30px; width: 30px; position: absolute; top: 45%; left: 50%;">
+<div class="cover"
+     style="position: absolute; top: 0; right: 0; left: 0; bottom: 0; background: url('<?= base_url('img/buy/buy_cover_after.jpg') ?>') #f6f6f6 center no-repeat; background-size: 100% auto;">
+    <div id="hand"
+         style="background: url('<?= base_url('img/hand.png') ?>') no-repeat; background-size: 30px 30px; height: 30px; width: 30px; position: absolute; top: 45%; left: 50%;">
     </div>
 </div>
 <script>
-    $("#hand").animate( { top: "+=6%" }, 500); 
-    $("#hand").animate( { top: "-=6%" }, 500); 
-    $("#hand").animate( { top: "+=6%" }, 500); 
-    $("#hand").animate( { top: "-=6%" }, 500); 
-    $('.cover').on('click', function (){
+    $('.cover').on('click', function () {
         $(this).fadeOut(500);
     });
 </script>
