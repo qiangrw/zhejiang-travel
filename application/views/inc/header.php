2a1abec0cf156f6cc1e705640c04ad42330a93f3
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 21:46:18 +0800

    delete icon

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>诗画浙江</title>
-    <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
     <script src="<?= base_url('js/jquery.min.js') ?>"></script>
 </head>
