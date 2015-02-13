f6c51ab4e1296f8b6b7b284da7942ff697fa90d5
Runwei Qiang <qiangrw@gmail.com>
2015-02-13 18:00:31 +0800

    fix bug

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -5,11 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>诗画浙江</title>
     <link rel="shortcut icon" href="favicon.ico">
-    <!--<link rel="stylesheet" href="<?= base_url('css/themes/default/jquery.mobile-1.4.5.min.css') ?>">-->
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
-    <!--<script src="<?= base_url('js/jquery.min.js') ?>"></script>-->
-    <script src="http://cdnjscn.b0.upaiyun.com/libs/jquery/1.11.1-rc2/jquery.min.js"</script>
-<!--    <script src="--><?//= base_url('js/jquery.mobile-1.4.5.min.js') ?><!--"></script>-->
+    <script src="<?= base_url('js/jquery.min.js') ?>"></script>
 </head>
 <body>
 <!--<img id="stay-img" src="--><?//= base_url('img/stay/stay_cover2.jpg') ?><!--" alt="" style="display: none"/>-->
